91c931544cf0dfb5ad7ac8ac5a603312435d112f
prakash deep sharma <42887999+prakashdeep2902@users.noreply.github.com>
2023-05-17 10:20:47 +0530

    done some changes

--- script.js
+++ script.js
@@ -1,46 +1,40 @@
 
 let jsonData;
 async function fetchData() {
+
     try {
         const response = await fetch("https://www.nobroker.in/prophub/wp-content/uploads/2022/05/dataSample.json");
         const data = await response.json();
         jsonData = data;
-   
         processData(jsonData);
-
     } catch (error) {
 
         console.error("Error:", error);
     }
 }
 
-
-let TableData=document.querySelector('table')
-
-let html = `     
-<tr class="row">
+let TableData = document.querySelector('table')
+let html =
+    `
+    <tr class="row">
     <th>Name</th>
     <th>Phone-No</th>
     <th>Age</th>
     <th>Country</th>
+    </tr>`
 
-</tr>
-
-`
 function processData(data) {
 
     data && data.map((ele) => {
 
         const { name, phone, age, country } = ele
-        html += 
-        `<tr>
+        html +=
+            `<tr>
             <td>${name}</td>
             <td>${phone}</td>
             <td>${age}</td>
             <td>${country}</td>
-
-        </tr>`
-
+            </tr>`
     })
 
     displayTableData(html);
@@ -50,4 +44,4 @@ function processData(data) {
 function displayTableData(html) {
     const TableData = document.querySelector("table");
     TableData.innerHTML = html.trim();
-  }
+}
